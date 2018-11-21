40e66608fab1a7f17b185cec4155d3ed6b8d61d7
sitaramshelke <sitaramshelke@gmail.com>
2018-11-21 07:41:45 +0530

    Add react-sandpack example code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import {
+  FileExplorer,
+  CodeMirror,
+  BrowserPreview,
+  SandpackProvider,
+} from 'react-smooshpack/es/components';
+
+const files = {
+  '/index.js': {
+    code: "<div>Hello There! Inside React-Sandbox</div>",
+  },
+};
+
+const dependencies = {
+  uuid: 'latest',
+};
+
+const App = () => (
+  <SandpackProvider files={files} dependencies={dependencies} entry="/index.js">
+    <div style={{ display: 'flex', width: '100%', height: '100%' }}>
+      <FileExplorer style={{ width: 300 }} />
+      <CodeMirror style={{ flex: 1 }} />
+      <BrowserPreview style={{ flex: 1 }} />
+    </div>
+  </SandpackProvider>
+);
 
 export default App;
